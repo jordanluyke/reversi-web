0f0d0cdb7c66d5efe2ac51c102f197321e092bae
Jordan Luyke <jordanluyke@gmail.com>
2018-10-15 17:43:04 -0700

    profile modal

--- client/src/app/partials/header/header.component.ts
+++ client/src/app/partials/header/header.component.ts
@@ -1,6 +1,7 @@
 import {Component, ViewChild, TemplateRef} from '@angular/core'
 import {ActivatedRoute} from '@angular/router'
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap'
+import {AccountService} from '../../../shared/index'
 
 @Component({
     selector: 'header-component',
@@ -10,10 +11,12 @@ import {NgbModal} from '@ng-bootstrap/ng-bootstrap'
 export class HeaderComponent {
 
     @ViewChild('aboutModal') public aboutModal: TemplateRef<any>
+    @ViewChild('profileModal') public profileModal: TemplateRef<any>
 
     constructor(
         private activedRoute: ActivatedRoute,
         private modalService: NgbModal,
+        public accountService: AccountService,
     ) {}
 
     public isPathActive(path: string): boolean {
@@ -22,5 +25,9 @@ export class HeaderComponent {
 
     public openAboutModal(): void {
         this.modalService.open(this.aboutModal)
+    }
+
+    public openProfileModal(): void {
+        this.modalService.open(this.profileModal)
     }
 }
